02a5d975c2fc22c04ed93522426b3aba6ceb647d
Matthew Bell <m.r.bell@outlook.com>
2022-10-15 23:41:54 +0100

    amended name checker

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,5 +1,14 @@
 function checkForName(inputText) {
   console.log("::: Running checkForName :::", inputText);
+
+  let regex =
+    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+
+  if (regex.test(inputText)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 export { checkForName };
